1b633d1f18368bb6157c17da1516ddd8665e84d9
Admin <reteamboss@gmail.com>
2024-09-23 14:22:47 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("Hello World!")
+    var array: IntArray = intArrayOf(2,34,5,6,7,8,34,45,65,67,87)
+
+    var result = with(array){
+        "Количество элементов в массиве: ${this.count()},Cумма элементов массива: ${this.sum()}"
+    }
+    println(result)
 }
